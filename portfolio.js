e9b5b40cf7f6de7be19413ff80f66f2ae531233e
Nathan Paskach <34314430+Paskach@users.noreply.github.com>
2022-09-04 19:42:54 -0500

    Added style

--- portfolio.js
+++ portfolio.js
@@ -4,36 +4,66 @@ var data = {
       "title":"About Me",
       "content":"My name is Nathan Paskach, and this is some placeholder text.",
       "images":[
-        ""
       ]
     },
     {
       "title":"Projects",
       "content":[
         {
+          "title":"Boxman",
+          "content":[
+			{
+			  "title":"Box Guy",
+			  "content":"In summer of 2021 I designed and built a physical programmer calculator, and this is some placeholder text.",
+			  "images":[
+			  ]
+			},
+			{
+			  "title":"Boxman CSE",
+			  "content":"In summer of 2021 I designed and built a physical programmer calculator, and this is some placeholder text.",
+			  "images":[
+			  ]
+			},
+			{
+			  "title":"Boxman CE",
+			  "content":"In summer of 2021 I designed and built a physical programmer calculator, and this is some placeholder text.",
+			  "images":[
+			  ]
+			},
+		  ],
+          "images":[
+          ]
+        },
+        {
           "title":"Programmer Calculator",
           "content":"In summer of 2021 I designed and built a physical programmer calculator, and this is some placeholder text.",
           "images":[
-            ""
           ]
         },
         {
           "title":"Go Kart",
           "content":"For 2 weeks in Summer 2020, my girlfriend Moriah and I built a go kart from junkyard scrap metal and a Honda generator engine we had lying around.",
           "images":[
-            ""
+            "images/go-kart/finished-go-kart.jpg",
+			"images/go-kart/chair-on-frame.jpg",
+			"images/go-kart/steering-wheel.jpg",
+			"images/go-kart/welding-bumper.jpg",
+			"images/go-kart/painting.jpg"
           ]
         },
         {
           "title":"Pocket BASIC Computer",
           "content":"Using a Z8671 processor, lots of wires, and a microcontroller to handle the screen and keyboard, I was able to build a small computer that runs Tiny BASIC.",
           "images":[
-            ""
+		    "images/basic-computer/finished-computer.jpg",
+			"images/basic-computer/keyboard-matrix.jpg",
+			"images/basic-computer/logic-board.jpg",
+			"images/basic-computer/logic-board-wiring.jpg",
+			"images/basic-computer/full-wiring.jpg"
           ]
         }
       ],
       "images":[
-        ""
       ]
     }
   ]
@@ -48,11 +78,13 @@ function fadeIn()
 {
 	if(opacity < 100)
   {
-  	opacity += 1;
+  	opacity += 2;
     cardContainers = document.getElementById('container').children;
     for(i = 0; i < cardContainers.length; i++)
     {
-    	cardContainers[i].children[0].style.opacity = opacity / 100;
+		cardContainers[i].style.opacity = opacity / 100;
+    	// cardContainers[i].children[0].style.opacity = opacity / 100;
+    	// cardContainers[i].children[0].children[0].style.opacity = opacity / 100;
     }
   }
   else
@@ -65,11 +97,13 @@ function fadeOut(list)
 {
 	if(opacity > 0)
   {
-  	opacity -= 1;
+  	opacity -= 2;
     cardContainers = document.getElementById('container').children;
     for(i = 0; i < cardContainers.length; i++)
     {
-    	cardContainers[i].children[0].style.opacity = opacity / 100;
+		cardContainers[i].style.opacity = opacity / 100;
+    	// cardContainers[i].children[0].style.opacity = opacity / 100;
+    	// cardContainers[i].children[0].children[0].style.opacity = opacity / 100;
     }
   }
   else
@@ -81,7 +115,7 @@ function fadeOut(list)
 
 function display(root)
 {
-	str = event.srcElement.parentNode.id;
+	str = event.srcElement.parentNode.parentNode.id;
 	root.forEach(o => 
   {
   	console.log(o);
@@ -110,6 +144,9 @@ function displayList(list)
     h = container.offsetHeight;
     aspect = w / h;
     numRows = Math.min(Math.ceil(Math.sqrt(len) / aspect), len);
+	numCols = Math.ceil(len / numRows);
+	if(numCols * numRows >= len + numCols)
+		numRows -= 1;
     cardWidth = Math.floor(w / Math.ceil(len / numRows));
     cardHeight = Math.floor(h / numRows);
     console.log({len, aspect, numRows, cardWidth, cardHeight});
@@ -123,10 +160,27 @@ function displayList(list)
       	if(i >= len)
         	continue;
       	var cc = temp.cloneNode(true);
-        var c = cc.children[0];
+        var cb = cc.children[0];
+        var c = cc.children[0].children[0];
         c.children[0].textContent = list[i]['title'];
         c.addEventListener('click', () => display(cards));
-        c.style.opacity = opacity;
+		cc.style.opacity = opacity;
+        //c.style.opacity = opacity;
+        //cb.style.opacity = opacity;
+		c.style.fontSize = cardHeight * 0.1 + 'px';
+		c.style.borderRadius = cardHeight * 0.05 + 'px';
+		cb.style.borderRadius = cardHeight * 0.05 + 'px';
+		if(list[i]['images'].length > 0)
+		{
+			cb.style.backgroundImage="url(" + list[i]['images'][0] + ")";
+			cb.style.backgroundSize = 'cover';
+			cb.style.backgroundPosition = 'center';
+			c.style.backdropFilter = 'brightness(50%) blur(4px)';
+		}
+		else
+		{
+			c.style.backgroundColor = '#aaccff';
+		}
         cc.style.left = col * cardWidth + 'px';
         cc.style.top = row * cardHeight + 'px';
         cc.style.width = cardWidth + 'px';
